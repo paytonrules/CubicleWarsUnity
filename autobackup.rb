4a1c8c6e09fcfd85dcf036a8dfcd350ffb458373
paytonrules <esmith@8thlight.com>
2013-04-24 22:07:03 -0500

    Add an autobackup script - thanks scott chacon

---
+++ autobackup.rb
@@ -0,0 +1,16 @@
+back_branch = 'refs/heads/backup'
+
+`rm /tmp/backup_index`
+ENV['GIT_INDEX_FILE'] = '/tmp/backup_index'
+
+last_commit = `git rev-parse #{back_branch}`.strip
+last_tree   = `git rev-parse #{back_branch}^{tree}`.strip
+
+`git add --all`
+next_tree = `git write-tree`.strip
+
+if last_tree != next_tree
+  extra = last_commit.size == 40 ? "-p #{last_commit}" : ''
+  csha = `echo 'back' | git commit-tree #{next_tree} #{extra}`
+  `git update-ref #{back_branch} #{csha}`
+end
