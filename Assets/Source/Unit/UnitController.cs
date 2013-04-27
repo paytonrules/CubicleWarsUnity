8a1606fbf90fe344b46c2cd05e417ff3d94f7e63
paytonrules <esmith@8thlight.com>
2013-04-27 00:10:16 -0500

    Bind to the Event

--- Assets/Source/Unit/UnitController.cs
+++ Assets/Source/Unit/UnitController.cs
@@ -3,25 +3,28 @@ using UnityEngine;
 using CubicleWarsLibrary;
 
 // The "controller" because it takes input from the game system
-public class UnitController : MonoBehaviour {
-  protected Unit unit;
-  protected StateMachine stateMachine;
+public class UnitController : MonoBehaviour
+{
+	protected Unit unit;
+	protected StateMachine stateMachine;
   
 	// 
-  void Start() 
-  {
-    stateMachine = GameRouter.stateMachine;
-  }
+	void Start ()
+	{
+		stateMachine = GameRouter.stateMachine;
+	}
 
 	// Bind is very simple
-  public void Bind(Unit unit) 
-  {
-    this.unit = unit;
-  }
+	public void Bind (Unit unit)
+	{
+		this.unit = unit;
+		this.unit.Died += () => DestroyObject (gameObject);
+	}
+	
 	// This selects a unit.  The Cubicle Wars state machine takes
 	// a game unit, not a unity object.
-	void OnMouseDown() 
-  	{
-		stateMachine.Select(unit);
+	void OnMouseDown ()
+	{
+		stateMachine.Select (unit);
 	}
 }
